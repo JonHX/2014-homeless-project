4ad6a7e7b0da69793ece10ebd26f5b4af4913d8f
Jon Tarrant <jon.tarrant@holidayextras.com>
2019-04-14 19:09:54 +0100

    Files found on old USB

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@
             }
         });
 });
-// bespoke function, if mobile device show placeholder 
+// bespoke function, if mobile device show placeholder
 $(document).ready(function() {
     if ($(window).width() < 700) { // if smaller then 1024px screen
          $('.mobile').show();
     }
-    else { 
+    else {
     $('.mobile').hide();
     }
-    
+
 });
 
   //  <!-- Scroll active class--> adds active class to whatever section the user is currently over. Found opensource on jsfiddle
@@ -53,31 +53,31 @@ $(document).ready(function() {
 $(document).ready(function() {
         $("body").sectionsnap({
         // time dilay (ms)
-        'delay': 100, 
+        'delay': 100,
 
         // selector
-        'selector': ".section", 
+        'selector': ".section",
 
         // % of window height from which we start
-        'reference': .9, 
+        'reference': .9,
 
         // animation time (snap scrolling)
-        'animationTime': 400, 
+        'animationTime': 400,
 
         // offset top (no snap before scroll reaches this position)
         'offsetTop': 0,
 
         // offset bottom (no snap after bottom - offsetBottom)
-        'offsetBottom': 0 
+        'offsetBottom': 0
         });
 });
 
 
 // section 1
-// 
+//
     //<!-- PLAY/PAUSE SWITCH --> created by enhancing small snippets online
-   
-    $(document).ready(function(){ //ready 
+
+    $(document).ready(function(){ //ready
         $("#switcher").click(function(){ //onclick #switcher
         imagePath = $("#switcher").attr("src"); // setting imagepath's src variable to #switcher
         if(imagePath == "img/icon/pause-icon.png"){ // if imagepaths src = pause button
@@ -86,23 +86,23 @@ $(document).ready(function() {
             $("#switcher").attr("src", "img/icon/pause-icon.png");  // else switch to pause button
         }
         });
-        
+
         $("#switcher2").click(function(){ // same as above with sound controls
         imagePath = $("#switcher2").attr("src");
         if(imagePath == "img/icon/unmute-icon.png"){
             $("#switcher2").attr("src", "img/icon/mute-icon.png");
         }else{
-            $("#switcher2").attr("src", "img/icon/unmute-icon.png");        
+            $("#switcher2").attr("src", "img/icon/unmute-icon.png");
         }
         });
     });
-        
+
 
 $(document).ready(function(){
 
     var quotes = $(".quotes"); // found technique on jsfiddle https://stackoverflow.com/questions/12065273/fade-in-out-text-loop-jquery .quotes variable
     var quoteIndex = -1;  // set quoteindex variable -1
-    
+
     function showNextQuote() { //function name
         ++quoteIndex; // ++ to quote variable
         quotes.eq(quoteIndex % quotes.length) // finds out how many .quotes and repeats in sequence
@@ -110,14 +110,14 @@ $(document).ready(function(){
             .delay(2000)
             .fadeOut(2000, showNextQuote); //repeat
     }
-    
+
     showNextQuote();
 
-    });  
+    });
 
-    
+
      //   <!-- PLAY/PAUSE BUTTON FOR VIDEO BACKGROUND--> create using small snippets from internet.
-   
+
         function vid_play_pause() { // this is a inline called function onclick of the front-end controls
           var myVideo = document.getElementById("bgvid");// get element bgvid and set to variable myVideo
           if (myVideo.paused) {      // if myvideo paused
@@ -126,15 +126,15 @@ $(document).ready(function(){
             myVideo.pause();
           }
         }
-        
-        function muteUnMute() // same as above but editing html5 attribute 
+
+        function muteUnMute() // same as above but editing html5 attribute
         { var myVideo = document.getElementById("bgvid");
             if( !myVideo.muted)
          myVideo.muted='muted';
         else
 
         myVideo.muted=false;
-        } 
+        }
 
  // onclick toggle bespoke made lightbox. Created within CSS
 $(document).ready(function(){
@@ -144,7 +144,7 @@ $(document).ready(function(){
         });
     });
 
-    
+
   //      <!--RSS FEEDS -->
  //   <!--Written by Jon Tarrant, this script changes what RSS feed is shown on click of the three icons. CSS height is also changed to fix a bug where inline styles were being applied dynmically to set the height to 0 when hidden. The .show() had no effect in recreating the height so I did it onclick also. It also adds an custom active class and removes it from every other link when clicked, to show which button is active. passing the active class did not work so custom active classes had to be made.-->
 
@@ -155,7 +155,7 @@ $(document).ready(function(){
         var element = document.getElementById("bg-link");
         element.classList.add("bg-link-active");
 
-      $('#bg-link').click(function(){ 
+      $('#bg-link').click(function(){
           var element = document.getElementById("bg-link");
           element.classList.add("bg-link-active");
           var element = document.getElementById("tw-link");
@@ -165,10 +165,10 @@ $(document).ready(function(){
           $('#blog-feed .rssincl-content').css({'height':'100px'});
           $('#blog-feed').show();
           $('#twitter-feed').hide();
-          $('#facebook-feed').hide();	
+          $('#facebook-feed').hide();
       });
 
-      $('#tw-link').click(function(){ 
+      $('#tw-link').click(function(){
           var element = document.getElementById("bg-link");
           element.classList.remove("bg-link-active");
           var element = document.getElementById("tw-link");
@@ -178,10 +178,10 @@ $(document).ready(function(){
           $('#blog-feed').hide();
           $('#twitter-feed .rssincl-content').css({'height':'100px'});
           $('#twitter-feed').show();
-          $('#facebook-feed').hide();	
+          $('#facebook-feed').hide();
       });
 
-      $('#fb-link').click(function(){ 
+      $('#fb-link').click(function(){
           var element = document.getElementById("bg-link");
           element.classList.remove("bg-link-active");
           var element = document.getElementById("tw-link");
@@ -191,25 +191,25 @@ $(document).ready(function(){
           $('#blog-feed').hide();
           $('#twitter-feed').hide();
           $('#facebook-feed .rssincl-content').css({'height':'100px'});
-          $('#facebook-feed').show();	
+          $('#facebook-feed').show();
       });
     });
-  
- 
+
+
 // section 2 options for bigSlide.js to show menu
 
         $(document).ready(function(){
             $('.slideContainer').hide();
-            
+
             $('.toggle').click(function () {
                 if ($(window).width() < 1024) { // if smaller then 1024px screen
                      $('#mapContainer').toggle('slide', { direction: 'right' }, 1000);
                       $('.slideContainer').toggle('slide', { direction: 'right' }, 1000);
                     }
-                else { 
+                else {
                 $('.slideContainer').toggle('slide', { direction: 'right' }, 1000);
                 }
-                 
+
             });
         });
 
@@ -224,20 +224,17 @@ $(document).ready(function(){
 });
 
 $(document).ready(function() {
-    
+
     // personal technique, responsive javascript margin-top
     $('.circles').click(function(){ // on click of circles
         if ($(window).width() < 1400 && $(window).width() > 1024) { // if smaller then 1400px screen
-            $('.circles').animate({ marginTop: "100px" }, 1000);  //margintop 150
+          $('.circles').animate({ marginTop: "100px" }, 1000);  //margintop 150
         }
-        else if ($(window).width() < 1024){
-          
-        }
-        else { // else // larger then 1400 margin top 280px;
-            $('.circles').animate({ marginTop: "280px" }, 1000);
+        else if (!$(window).width() < 1024){
+          $('.circles').animate({ marginTop: "70px" }, 1000);
         }
     });
-    
+
     // on click of #content-1 increase circle size, show hidden content hide all other circle content. Personal technique
         $("#content-1").click(function () {
 
@@ -260,7 +257,7 @@ $(document).ready(function() {
 
             $('.collapse-2').slideToggle(700, "jswing", function () {
             });
-            
+
             $('.collapse-1').fadeOut(100);
             $('.collapse-3').fadeOut(100);
             $('.collapse-4').fadeOut(100);
@@ -278,7 +275,7 @@ $(document).ready(function() {
 
             $('.collapse-3').slideToggle(700, "jswing", function () {
             });
-            
+
             $('.collapse-1').fadeOut(100);
             $('.collapse-2').fadeOut(100);
             $('.collapse-4').fadeOut(100);
@@ -296,7 +293,7 @@ $(document).ready(function() {
 
             $('.collapse-4').slideToggle(700, "jswing", function () {
             });
-            
+
             $('.collapse-1').fadeOut(100);
             $('.collapse-2').fadeOut(100);
             $('.collapse-3').fadeOut(100);
@@ -307,14 +304,14 @@ $(document).ready(function() {
             $('#content-4').animate({ width: "250px", height: "250px" }, {duration: 500, queue: false});
             }
             else {}
-   
+
         });
     });
 
-// section 4 
+// section 4
 // personal technique, on click of each div ID, show add the 'manClick' class which will increase the outer-div width so that the two graphics associated with that div will show without fault.
 $(document).ready(function() {
- $('#alcohol').click(function(){ 
+ $('#alcohol').click(function(){
      $('.healthCheck1').toggleClass('manClick');
      $('.healthCheck2').removeClass('manClick');
      $('.healthCheck3').removeClass('manClick');
@@ -345,7 +342,7 @@ $(document).ready(function() {
      $('#graphic-4').toggleClass('animated fadeInRight clicked');
        });
 
-   $('#ambulance').click(function(){ 
+   $('#ambulance').click(function(){
      $('.healthCheck3').toggleClass('manClick');
      $('.healthCheck1').removeClass('manClick');
      $('.healthCheck2').removeClass('manClick');
@@ -360,7 +357,7 @@ $(document).ready(function() {
      $('#graphic-6').toggleClass('animated fadeInRight clicked');
        });
 
- $('#mental').click(function(){ 
+ $('#mental').click(function(){
      $('.healthCheck4').toggleClass('manClick');
      $('.healthCheck1').removeClass('manClick');
      $('.healthCheck2').removeClass('manClick');
@@ -381,7 +378,7 @@ $(document).ready(function() {
 $(document).ready(function() {
      $('#nav11').addClass('active'); // auto add class to section 1 onload
     $(document).mousemove(function() {
-        if ($('#nav1').hasClass("active")) { // if nav1 (section 1) has .active 
+        if ($('#nav1').hasClass("active")) { // if nav1 (section 1) has .active
             $('#nav11').addClass('active'); // add .active to the corresponding .miniNav link
 
         } else {                            // else remove class active, this is important to keep the active class updated.
